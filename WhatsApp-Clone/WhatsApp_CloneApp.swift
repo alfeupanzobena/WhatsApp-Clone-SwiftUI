53156b3ec64f902f64981cba27561c1790d9b544
alfeupanzobena <alfeupanzobena28@gmail.com>
2024-12-26 11:41:25 +0100

    Initial Commit

---
+++ WhatsApp-Clone/WhatsApp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_CloneApp.swift
+//  WhatsApp-Clone
+//
+//  Created by Alfeu Panzo Bena on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
